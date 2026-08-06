67c34ba550380c2ada5ea8e683270d6007d5fc7e
naseeraslam <naseeraslam4546@gmail.com>
2026-08-06 19:19:27 +0500

    gtm_analytics_integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Lato, JetBrains_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import LenisProvider from '@/components/common/LenisProvider'
 import CustomCursor from '@/components/common/CustomCursor'
@@ -189,6 +190,20 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${lato.variable} ${jetbrains.variable} scroll-smooth`}>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WF4FP3J5');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdPerson) }}
@@ -203,6 +218,17 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-[#060911] text-slate-100 antialiased selection:bg-orange-500 selection:text-white">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WF4FP3J5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <LenisProvider>
           <ReadingProgressBar />
           <CustomCursor />
