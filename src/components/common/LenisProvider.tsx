c89b42bfefd7d8033e2ab712f38f7dd3df1e92c7
naseeraslam <naseeraslam4546@gmail.com>
2026-08-06 19:32:06 +0500

    lenis_idle_optimization

--- src/components/common/LenisProvider.tsx
+++ src/components/common/LenisProvider.tsx
@@ -1,27 +1,47 @@
 'use client'
 
 import { ReactNode, useEffect } from 'react'
-import Lenis from 'lenis'
 
 export default function LenisProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-    })
+    // Disable Lenis on mobile/touch devices to prevent main thread layout locking
+    if (window.matchMedia('(pointer: coarse)').matches) return
 
-    function raf(time: number) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
+    let lenisInstance: any
+    let rafId: number
+    let timeoutId: NodeJS.Timeout
+
+    const initLenis = async () => {
+      const { default: Lenis } = await import('lenis')
+      lenisInstance = new Lenis({
+        duration: 1.0,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 1.5,
+      })
+
+      function raf(time: number) {
+        if (lenisInstance) {
+          lenisInstance.raf(time)
+          rafId = requestAnimationFrame(raf)
+        }
+      }
+
+      rafId = requestAnimationFrame(raf)
     }
 
-    requestAnimationFrame(raf)
+    if ('requestIdleCallback' in window) {
+      // @ts-ignore
+      window.requestIdleCallback(initLenis)
+    } else {
+      timeoutId = setTimeout(initLenis, 300)
+    }
 
     return () => {
-      lenis.destroy()
+      if (timeoutId) clearTimeout(timeoutId)
+      if (rafId) cancelAnimationFrame(rafId)
+      if (lenisInstance) lenisInstance.destroy()
     }
   }, [])
 
